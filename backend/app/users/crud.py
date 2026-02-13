1ea8cd49f021d35218d8119847a4be4dadd508e0
akshay07-max <akshaysurase992@gmail.com>
2026-02-13 22:58:19 +0530

    Profile crud

--- backend/app/users/crud.py
+++ backend/app/users/crud.py
@@ -4,3 +4,9 @@ def get_profile(uid: str):
     doc = db.collection("users").document(uid).get()
     return doc.to_dict()
 
+def update_profile(uid: str, data: dict):
+    db.collection("users").document(uid).update(data)
+
+def delete_profile(uid:str):
+    db.collection("users").document(uid).delete()
+
