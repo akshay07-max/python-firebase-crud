b325ed25ca24b3f95a3db822508b629c1802be6b
akshay07-max <akshaysurase992@gmail.com>
2026-02-13 09:36:01 +0530

    login api

--- backend/app/auth/login.py
+++ backend/app/auth/login.py
@@ -3,3 +3,28 @@ from firebase_admin import auth
 from auth.jwt_handler import create_access_token
 
 FIREBASE_API_KEY = "YOUR_FIREBASE_API_KEY"
+
+def login_user(email:str, password:str):
+    url=("https://identitytoolkit.googleapis.com/v1/"f"accounts:signInWithPassword?key={FIREBASE_API_KEY}")
+
+    payload = {
+        "email": email,
+        "password": password,
+        "returnSecureToken": True
+    }
+
+    res = requests.post(url, json=payload)
+
+    if res.status_code != 200:
+        raise Exception("Invalid Credentials")
+    
+    firebase_token= res.json()["idToken"]
+    decoded = auth.verify_id_token(firebase_token)
+
+    access_token = create_access_token({
+        "uid": decoded["uid"],
+        "email": decoded["email"],
+        "role": decoded.get("role")
+    })
+
+    return access_token
